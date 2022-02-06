a327af0c58e195b8f0a0ad1e1085b5799eb2d8b2
Rrrrvo <72281884+Rrrrvo@users.noreply.github.com>
2022-02-06 13:59:02 +0200

    Сделал потолок и настроил немного постпроццесинг

---
+++ Assets/Scripts/LightIntensity.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightIntensity : MonoBehaviour
+{
+    public float duration = 1.0F;
+    public Light lt;
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+    void Update()
+    {
+        float phi = Time.time / duration * 2 * Mathf.PI;
+        float amplitude = Mathf.Cos(phi) * 0.5F + 0.5F;
+        lt.intensity = amplitude;
+    }
+
+
+}
